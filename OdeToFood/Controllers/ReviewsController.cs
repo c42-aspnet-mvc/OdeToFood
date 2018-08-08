158a4556502150000536853c9e745f113a34fadc
c42-arun <cloud.fortytwo@gmail.com>
2018-08-08 17:02:31 +0100

    Reviews link added to Restaurants' Index view (navigates to /Reviews/Index)

---
+++ OdeToFood/Controllers/ReviewsController.cs
@@ -0,0 +1,35 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        OdeToFoodDb _db = new OdeToFoodDb();
+
+        //
+        // GET: /Reviews/
+
+        public ActionResult Index([Bind(Prefix = "id")] int restaurantId)
+        {
+            var restaurant = _db.Restaurants.Find(restaurantId);
+
+            if (restaurant == null)
+                return new HttpNotFoundResult();
+
+            return View(restaurant);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_db != null)
+                _db.Dispose();
+
+            base.Dispose(disposing);
+        }
+    }
+}
