5de33c280a93b095cad3ee3718e8fe24b6a4efd9
c42-arun <cloud.fortytwo@gmail.com>
2018-09-16 09:13:37 +0100

    Added new bundle config for all jQuery scripts; EF Seed method with 1000 dummy restaurants

--- OdeToFood/Migrations/Configuration.cs
+++ OdeToFood/Migrations/Configuration.cs
@@ -17,7 +17,7 @@ namespace OdeToFood.Migrations
         protected override void Seed(OdeToFoodDb context)
         {
             context.Restaurants.AddOrUpdate(r => r.Name,
-                new Restaurant { Name = "Sabatino's", City = "Baltimore", Country = "USA"},
+                new Restaurant { Name = "Sabatino's", City = "Baltimore", Country = "USA" },
                 new Restaurant { Name = "Great Lake", City = "Chicago", Country = "USA" },
                 new Restaurant
                 {
@@ -30,6 +30,12 @@ namespace OdeToFood.Migrations
                     }
                 }
                 );
+
+            for (int i = 0; i < 1000; i++)
+            {
+                context.Restaurants.AddOrUpdate(r => r.Name,
+                    new Restaurant { Name = i.ToString(), City = "Nowhere", Country = "UK" });
+            }
         }
     }
 }
